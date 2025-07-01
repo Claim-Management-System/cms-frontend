853f6e51f1c35f594507c506ddfe8d56768278e3
MuhaibShamsher <muhaibshamsher9999@gmail.com>
2025-07-01 01:35:02 -0700

    Integrate AuthProvider and RouterProvider for routing and authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-
+import { RouterProvider } from 'react-router-dom';
+import { AuthProvider } from './context/authContext';
+import { router } from './routes/Router';
 
 function App() {
-
   return (
-
-  )
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
