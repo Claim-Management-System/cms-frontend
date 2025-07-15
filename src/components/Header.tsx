effb4bdfcdf599a0e4463900cc2f844e6fee220e
MuhaibShamsher <muhaibshamsher9999@gmail.com>
2025-07-15 15:12:34 -0700

    Add Header component with responsive layout and notification icons

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, IconButton, Box } from "@mui/material"
+import { Notifications, AccountCircle } from "@mui/icons-material"
+
+interface HeaderProps {
+  pageName: string;
+}
+
+function Header({ pageName }: HeaderProps) {
+
+  return (
+    <AppBar
+      position="static"
+      elevation={0}
+      sx={{ bgcolor: "white", color: "text.primary", backgroundColor: "#f9fafb"}}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Typography variant="h5" sx={{ fontWeight: 300, height: 40 }}>
+          {pageName}
+        </Typography>
+
+        <Box sx={{ height: 40, display: "flex", alignItems: "center", gap: { xs: 0, md: 2 } }}>
+          <IconButton sx={{ width: 32, height: 32 }}>
+            <Notifications />
+          </IconButton>
+
+          <IconButton sx={{ width: 32, height: 32 }}>
+            <AccountCircle />
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default React.memo(Header);
