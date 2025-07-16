1f1a854c8cc18014f857c4b134a70379af1e5ed2
MuhaibShamsher <muhaibshamsher9999@gmail.com>
2025-07-16 12:34:41 -0700

    Implement Miscellaneous page with claims fetching, search, and pagination functionality

--- src/pages/claim_requests/Miscellaneous.tsx
+++ src/pages/claim_requests/Miscellaneous.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box } from '@mui/material'
+import Header from '../../components/Header'
+import SearchBox from '../../components/SearchBox'
+import ClaimTable from '../../components/claimsTable/ClaimTable'
+import Pagination from '../../components/Pagination'
+import { useError } from '../../context/errorContext';
+import { getClaims } from '../../services/dataServices/claimRequests'
 
-function Miscellaneous() {
-  return (
-    <div>Miscellaneous</div>
-  )
+export default function Miscellaneous() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [claimData, setClaimData] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const { setError } = useError();
+
+    const fetchAllClaims = async () => {
+        setIsLoading(true);
+        try {
+            const data = await getClaims(searchTerm, currentPage);
+            setClaimData(data);
+        } catch (error: any) {
+            setError(error?.message || 'Failed to fetch claims');
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchAllClaims();
+    }, [currentPage, searchTerm]);
+
+    return (
+        <Box sx={{ marginX: 3 }}>
+            <Header pageName='Claim Requests / MISC. EXPENSES' />
+
+            <Box sx={{ display: 'flex', justifyContent: 'flex-start', paddingX: 3, marginBottom: 3 }}>
+                <SearchBox onSearchChange={setSearchTerm} />
+            </Box>
+
+            <ClaimTable
+                data={claimData}
+                userRole="admin"
+                claimType="misc"
+                category="claim requests"
+                loading={isLoading}
+            />
+
+            <Pagination currentPage={currentPage} totalPages={10} onPageChange={setCurrentPage} />
+        </Box>
+    )
 }
-
-export default Miscellaneous
