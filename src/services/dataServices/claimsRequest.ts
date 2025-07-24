70571f973bbc9ae08780b6f269e9a03f334d837f
MuhaibShamsher <muhaibshamsher9999@gmail.com>
2025-07-24 07:36:38 -0700

    Refactor claimsRequest.ts by improving formatting and adding postNewRequest function for submitting claims

--- src/services/dataServices/claimsRequest.ts
+++ src/services/dataServices/claimsRequest.ts
@@ -6,7 +6,7 @@ type getClaimsRequestProps = {
   page: number;
 }
 
-export const getClaimsRequest = async ({claimType, search, page}: getClaimsRequestProps) => {
+export const getClaimsRequest = async ({ claimType, search, page }: getClaimsRequestProps) => {
   try {
     const response = await apiClient.get('/api/claims', {
       params: { claimType, page, search, status: 'pending' },
@@ -16,3 +16,46 @@ export const getClaimsRequest = async ({claimType, search, page}: getClaimsReque
     throw error;
   }
 };
+
+
+type postNewRequestProps = {
+  user_id: string,
+  employee_number: number,
+  claim_type_id: number,
+  title: string,
+  description: string,
+  purpose_of_visit?: string,
+  relationship: string,
+  submitted_amount: number,
+  month: string,
+  images: File[]
+}
+
+export const postNewRequest = async (formData: postNewRequestProps) => {
+  try {
+    const data = new FormData();
+     Object.keys(formData).forEach(key => {
+      const typedKey = key as keyof postNewRequestProps;
+      
+      if (typedKey !== 'images') {
+        data.append(typedKey, String(formData[typedKey]));
+      }
+    });
+    if (formData.images && formData.images.length > 0) {
+      formData.images.forEach(file => {
+        data.append('images', file);
+      });
+    }
+
+    const response = await apiClient.post('/api/claims', data, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+
+    console.log('Claim submitted successfully:', response);
+    return response.data;
+  } catch (error: any) {
+    throw error;
+  }
+}
