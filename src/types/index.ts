f9749294ea59b5949023b3471eb47e4fd3ef9705
MuhaibShamsher <muhaibshamsher9999@gmail.com>
2025-07-24 07:30:14 -0700

    Remove unused types and interfaces from types.ts file

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,3 +9,25 @@ export interface ClaimRecord {
   claim_type?: string
   relationship?: string;
 }
+
+export type FormType =
+    | "MISCELLANEOUS EXPENSE FORM"
+    | "OUT PATIENT CLAIM FORM";
+
+export interface MiscFormData {
+    title: string;
+    itemType: string;
+    otherItemType?: string; // NEW: To store the custom value for "Other"
+    description: string;
+    totalAmount: string;
+}
+
+export interface OpdFormData {
+    title: string;
+    patientName: string;
+    relationship: string;
+    purposeOfVisit: string;
+    otherPurposeOfVisit?: string; // NEW: To store the custom value for "Other"
+    expenseType: string;
+    totalAmount: string;
+}
