eae785578a95eb60b39dd746241f6e9563049db8
MuhaibShamsher <muhaibshamsher9999@gmail.com>
2025-07-29 01:14:58 -0700

    Add constants for user roles, claim types, and status

---
+++ src/services/constantServices/constants.ts
@@ -0,0 +1,15 @@
+export const USER_ROLES = Object.freeze({
+    ADMIN: 'admin',
+    USER: 'user'
+});
+
+export const CLAIM_TYPES = Object.freeze({
+    MISC: 'miscellaneous',
+    OPD: 'outpateint'
+});
+
+export const STATUS = Object.freeze({
+  APPROVED: 'approved',
+  PENDING: 'pending',
+  REJECTED: 'rejected',
+});
