caf55fcde54abcea9d4d0bf7b8d9e8d40a7ad604
MuhaibShamsher <muhaibshamsher9999@gmail.com>
2025-07-30 06:08:53 -0700

    Add Employee Interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -22,3 +22,27 @@ export interface ClaimCounts {
 export type UserRole = 'admin' | 'user';
 export type ClaimCategory = 'claim history' | 'claim requests';
 export type ClaimType = 'miscellaneous' | 'outpatient';
+
+export type Role = 'admin' | 'employee';
+export type EmployeeType = 'permanent' | 'contractual';
+export type MaritalStatus = 'single' | 'married' | 'family';
+
+export interface EmployeeInterface {
+    firstName: string;
+    lastName: string;
+    email: string;
+    dob: string;
+    joiningDate: string;
+    role: Role | '';
+    employeeType: EmployeeType | '';
+    team: string;
+    bankAccountNumber: string;
+    employeeId: string;
+    maritalStatus: MaritalStatus | '';
+    workLocation: string;
+    jobTitle: string;
+    position: string;
+    phoneNumber: string;
+    age?: number;
+    password?: string;
+}
