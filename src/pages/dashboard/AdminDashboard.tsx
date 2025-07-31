c9ebf92709c898e537099a4f75fb23d1f246df8b
Taha Munawar <109829503+tahamunawar@users.noreply.github.com>
2025-07-31 15:08:11 +0500

    Refactor style

--- src/pages/dashboard/AdminDashboard.tsx
+++ src/pages/dashboard/AdminDashboard.tsx
@@ -1,26 +1,42 @@
 import React, { useState } from 'react';
-import './AdminDashboard.css';
 import UserSearch from '../../components/userSearch/UserSearch';
 import DetailsTable from '../../components/detailsTable/DetailsTable';
 import Header from '../../components/Header';
+import UserTitle from '../../components/userTitle/UserTitle';
 
 const AdminDashboard = () => {
   const [isSearched, setIsSearched] = useState(false);
 
-  const internetClaimData = [
-    { description: 'Approved Internet Claims', amount: '17168' },
-    { description: 'Pending Internet Claims', amount: '0' },
+  const claimDetailsData = [
+    {
+      type: 'Internet',
+      approvedAmount: '17168',
+      approvedCount: 0,
+      pendingAmount: '0',
+      pendingCount: 0,
+    },
+    {
+      type: 'Medical',
+      approvedAmount: '62058',
+      approvedCount: 0,
+      pendingAmount: '0',
+      pendingCount: 0,
+    },
+    {
+      type: 'Miscellaneous',
+      approvedAmount: 0,
+      approvedCount: 0,
+      pendingAmount: 0,
+      pendingCount: 0,
+    },
   ];
 
-  const medicalClaimData = [
-    { description: 'Approved Medical Claims', amount: '62058' },
-    { description: 'Pending Medical Claims', amount: '0' },
-  ];
-
-  const medicalLimitData = [
-    { description: 'Medical Limit', amount: '116667' },
-    { description: 'Consumed Medical Amount', amount: '62058' },
-    { description: 'Remaining Amount', amount: '54609' },
+  const claimDetailsColumns = [
+    { key: 'type', header: 'Type' },
+    { key: 'approvedAmount', header: 'Approved Claim Amount' },
+    { key: 'approvedCount', header: 'Approved Count' },
+    { key: 'pendingAmount', header: 'Pending Claim Amount' },
+    { key: 'pendingCount', header: 'Pending Count' },
   ];
 
   const handleSearch = () => {
@@ -29,17 +45,16 @@ const AdminDashboard = () => {
 
   return (
     <div>
-      <Header pageName='Admin Dashboard' />
+      <Header pageName="Admin Dashboard" />
       <UserSearch onSearch={handleSearch} />
       {isSearched && (
         <>
-          <div className="employee-details">
-            <p>Employee ID: 12345</p>
-            <p>Employee Name: John Doe</p>
-          </div>
-          <DetailsTable title="Internet Claim Details" data={internetClaimData} />
-          <DetailsTable title="Medical Claim Details" data={medicalClaimData} />
-          <DetailsTable title="Medical Limit Details" data={medicalLimitData} />
+          <UserTitle mainText="John Doe" subText="Employee ID: 12345" />
+          <DetailsTable
+            title="Claim Details"
+            data={claimDetailsData}
+            columns={claimDetailsColumns}
+          />
         </>
       )}
     </div>
