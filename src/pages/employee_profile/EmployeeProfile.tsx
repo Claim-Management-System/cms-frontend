5320d409fb31853c568d6a1fbe5e42898d9bafc4
MuhaibShamsher <muhaibshamsher9999@gmail.com>
2025-08-02 15:37:14 +0500

    profile page for viewing the employee details

---
+++ src/pages/employee_profile/EmployeeProfile.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import ProfileTable from '../../components/profile/ProfileTable';
+import UserTitle from '../../components/userTitle/UserTitle';
+import Header from '../../components/Header';
+import { fetchProfile } from '../../utils/userProfileUtils';
+import { useAuth } from '../../context/authContext';
+import { useError } from '../../context/errorContext';
+import type { ProfileSection } from '../../types';
+import './EmployeeProfile.css';
+
+
+const EmployeeProfile = () => {
+    const [employeeDetails, setEmployeeDetails] = useState<ProfileSection[]>([]);
+
+    const { employeeId } = useParams();
+    const { user } = useAuth();
+    const { setError } = useError();
+
+    const fetchEmployeeProfileDetails = async (employeeId: number) => {
+        try {
+            const data: ProfileSection[] = await fetchProfile(employeeId)
+            setEmployeeDetails(data)
+        } catch (error: any) {
+            setError(error.message);
+        }
+    }
+
+    useEffect(() => {
+        if(employeeId) {
+            fetchEmployeeProfileDetails(Number(employeeId))
+        } else {
+            setError('Employee ID not exists!')
+        }
+    }, [employeeId])
+
+
+    return (
+        <>
+            <Header pageName='User Profile' />
+            <div className="user-profile-container">
+                <UserTitle mainText={user?.employee_name!} subText={`Employee ID: ${employeeId}`} />
+                {employeeDetails.map(employeeDetail => (
+                    <ProfileTable
+                        key={employeeDetail.title}
+                        title={employeeDetail.title}
+                        details={employeeDetail.details}
+                    />
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default EmployeeProfile;
